74ad58a97f62211a0923c595f615fd1510643b24
Yash Gupta <yashg.bits@gmail.com>
2022-01-25 10:35:16 +0530

    updated docker network

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,29 +4,34 @@ const user = require('../Controllers/userController')
 
 //add a user 
 router.post('/adduser', (req, res) => {
+    console.log("/adduseriscalled");
     user.createUser(req, res)
 })
 
 //show a user 
 
 router.post('/find', (req, res) => {
+    console.log("/findiscalled");
     user.finduser(req, res)
 })
 
 // show all the users
 
 router.get('/listusers', (req, res) => {
+    console.log("listusersiscalled");
     user.showUsers(req, res)
 })
 
 // update user 
 
 router.post('/updateuser', (req, res) => {
+    console.log("/updateuseriscalled");
     user.updateUser(req, res)
 })
 
 //delete user 
 router.delete('/deleteuser', (req, res) => {
+    console.log("/deleteuseriscalled");
     user.delete(req, res)
 })
 
