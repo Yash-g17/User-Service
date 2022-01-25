7536bda34f2b421c827c3c6d3545f97d73c1c039
Yash Gupta <yashg.bits@gmail.com>
2022-01-25 15:34:53 +0530

    added cron job

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require('./models/db')
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const cron = require("node-cron")
 
 const app = express();
 const UserRouter = require('./routes/userRoutes')
 
 app.use(bodyParser.json())
+cron.schedule("* 0 * * *", function () {
+    console.log("service is up and running ");
+});
 
 app.use('/user', UserRouter)
 app.get('/', (req, res) => {
