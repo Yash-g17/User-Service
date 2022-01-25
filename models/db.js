f7eda31bdf1f10357967e39e49e705052290beac
Yash Gupta <yashg.bits@gmail.com>
2022-01-25 14:43:42 +0530

    updated mongo container

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://mongo:27017/UserDB', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb://mongouser:27017/UserDB', { useNewUrlParser: true }, (err) => {
     if (!err) console.log("connected mongodb");
     else console.log('error in connection ' + error);
 })
